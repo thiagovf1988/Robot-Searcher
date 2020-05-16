a084be9d0ba270bd6d962be7831816434482367f
thiagovf1988 <61379143+thiagovf1988@users.noreply.github.com>
2020-05-16 17:55:19 +0100

    Updates

---
+++ precache-manifest.95b5977dc2f85da1cf94f2dea4f6a8c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c039b01b8b41c274d6e274b390946b",
+    "url": "/mprotagonist/index.html"
+  },
+  {
+    "revision": "2deeb3fc279421eea453",
+    "url": "/mprotagonist/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "71a531e413dca590a3e8",
+    "url": "/mprotagonist/static/css/main.50b549df.chunk.css"
+  },
+  {
+    "revision": "2deeb3fc279421eea453",
+    "url": "/mprotagonist/static/js/2.09e6005a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mprotagonist/static/js/2.09e6005a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a531e413dca590a3e8",
+    "url": "/mprotagonist/static/js/main.d6f44faa.chunk.js"
+  },
+  {
+    "revision": "893a9ba34ee48b96e99e",
+    "url": "/mprotagonist/static/js/runtime-main.69273d7e.js"
+  },
+  {
+    "revision": "d749ab74b876d5c52256c0559b487aff",
+    "url": "/mprotagonist/static/media/Pokemon Hollow.d749ab74.woff"
+  },
+  {
+    "revision": "4197aed63a267f9ae61f800cda823803",
+    "url": "/mprotagonist/static/media/Pokemon Solid.4197aed6.woff"
+  }
+]);
