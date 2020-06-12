a50b48874aeb516d1b337d2daf6c83f3669b3e28
thiagovf1988 <thiagovf@LAPTOP-1FU0JGM9.localdomain>
2020-06-12 19:47:01 +0100

    changes from linux

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Card = ({ name, email, id }) => {
-  const ff = id + 255;
+  const ff = id + 255;//rand
   return (
     <div className="tc bg-lightest-blue dib br3 pa2 ma2 grow bw2 shadow-5">
       <img alt="robots" src={`https://robohash.org/${ff}?200x200`} />
